3fda4e3b2f6233cfa63718934fa457164a4f3b6b
Kerberos9 <mll.kerberos@gmail.com>
2021-11-25 16:37:27 +0100

    Initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import Checklist from '../Checklist/Checklist';
+import './App.css';
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      checklists: JSON.parse(localStorage.getItem('checklists')) || [],
+      currentChecklist: localStorage.getItem('currentChecklist') || null
+    };
+  }
+
+  importChecklist = () => {
+    let text = this.state.textArea;
+    let lines = text.split('\n');
+    let checklist = {
+      title: this.state.checklistTitle,
+      steps: []
+    };
+    lines.map(l => checklist.steps.push({ text: l, completed: false }));
+    let checklists = this.state.checklists;
+    checklists.push(checklist);
+
+    localStorage.setItem('checklists', JSON.stringify(checklists));
+    let currentChecklist = this.state.currentChecklist || 0;
+    localStorage.setItem('currentChecklist', currentChecklist);
+    this.setState({ checklists, currentChecklist });
+  };
+
+  checklistContentHandle = e => {
+    this.setState({ textArea: e.target.value });
+  };
+
+  checklistTitleHandle = e => {
+    this.setState({ checklistTitle: e.target.value });
+  };
+
+  changeChecklist = e => {
+    this.setState({
+      currentChecklist: e.target.value,
+      checklist: this.state.checklists[e.target.value]
+    });
+    localStorage.setItem('currentChecklist', e.target.value);
+  };
+
+  updateStep = i => {
+    let checklists = this.state.checklists;
+    checklists[this.state.currentChecklist].steps[i].completed ^= true;
+    this.setState({ checklists });
+    localStorage.setItem('checklists', JSON.stringify(checklists));
+  };
+
+  render() {
+    return (
+      <div>
+        <div className='checklist-import-inputs'>
+          <span>Title</span>
+          <input type='text' onChange={e => this.checklistTitleHandle(e)} />
+          <span>Text</span>
+          <textarea
+            cols='5'
+            rows='2'
+            onChange={e => this.checklistContentHandle(e)}></textarea>
+        </div>
+        <button onClick={this.importChecklist.bind(this)}>Import</button>
+
+        <div className='checklist-selection'>
+          <select
+            value={this.state.currentChecklist}
+            onChange={e => this.changeChecklist(e)}>
+            {this.state.checklists
+              ? this.state.checklists.map((c, i) => (
+                  <option name='checklist' value={i} key={i}>
+                    {c.title}
+                  </option>
+                ))
+              : 'a'}
+          </select>
+        </div>
+        <Checklist
+          data={this.state.checklists[this.state.currentChecklist]}
+          updateStep={i => this.updateStep(i)}
+        />
+      </div>
+    );
+  }
+}
---
+++ src/components/Checklist/Checklist.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import './Checklist.css';
+const removeMd = require('remove-markdown');
+export default class Checklist extends Component {
+  toggleStep = (e, i) => {
+    e.target.classList.toggle('completed');
+    this.props.updateStep(i);
+  };
+
+  render() {
+    return (
+      <div className='checklist'>
+        <h1 className='title'>
+          {this.props.data ? this.props.data.title : ''}
+        </h1>
+        <div className='checklist-steps'>
+          {this.props.data
+            ? this.props.data.steps.map((s, i) => {
+                if (s.text.substr(0, 2) === '##') {
+                  return (
+                    <h3
+                      className={`checklist-step ${
+                        s.completed ? 'completed' : ''
+                      }`}
+                      key={i}
+                      onClick={e => this.toggleStep(e, i)}>
+                      {removeMd(s.text)}
+                    </h3>
+                  );
+                } else if (s.text.substr(0, 1) === '#') {
+                  return (
+                    <h1
+                      className={`checklist-step ${
+                        s.completed ? 'completed' : ''
+                      }`}
+                      key={i}
+                      onClick={e => this.toggleStep(e, i)}>
+                      {removeMd(s.text)}
+                    </h1>
+                  );
+                } else {
+                  return (
+                    <p
+                      className={`checklist-step ${
+                        s.completed ? 'completed' : ''
+                      }`}
+                      key={i}
+                      onClick={e => this.toggleStep(e, i)}>
+                      {removeMd(s.text)}
+                    </p>
+                  );
+                }
+              })
+            : 'a'}
+        </div>
+      </div>
+    );
+  }
+}
